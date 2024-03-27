2674a525d3cdce647f4969e39186b250b9985a3b
SEBASTIEN_VAN CLEMPUTTE <sebastienvanclemputte@gmail.com>
2024-03-27 13:47:36 +0100

    components.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {} from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <PokemonCard />
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,21 @@
 function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-      <figcaption>Bulbasaur</figcaption>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+      <figcaption> {pokemon.name}</figcaption>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
     </figure>
   );
 }
